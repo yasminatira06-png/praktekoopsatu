c984bb8ffaf7aadd3084f157c02971e364e3ee25
yasminatira06-png <yasminatira06@gmail.com>
2025-10-03 00:49:58 +0700

    Delete aksesoris.php

--- aksesoris.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Models;
-use Core\Produk;
-
-class Aksesoris extends Produk {
-    public function info(): string {
-        return "Aksesoris: {$this->nama}, Harga: Rp {$this->getHarga()}";
-    }
-}
