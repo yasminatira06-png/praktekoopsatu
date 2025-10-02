f0a3e5b63e6e0809409e85dff592d8bedddb47bc
yasminatira06-png <yasminatira06@gmail.com>
2025-10-03 00:50:35 +0700

    Delete pelanggan.php

--- pelanggan.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Models;
-
-final class Pelanggan {
-    private string $nama;
-    public function __construct(string $nama) { $this->nama = $nama; }
-    public function getNama(): string { return $this->nama; }
-}
